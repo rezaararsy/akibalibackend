57f2f5857848a5ed30874282d84211765941d794
Reza Ararsy <rezaararsy@gmail.com>
2019-11-19 23:11:51 +0700

    Admin Dashboard Done

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -62,27 +62,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		margin: 10px;
 		border: 1px solid #D0D0D0;
 		box-shadow: 0 0 8px #D0D0D0;
+		padding:100;
 	}
 	</style>
 </head>
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+<div id="">
+	<h1>Diisi Frontend</h1>
+	Akses Adminnya Klik <a href="http://localhost/akibali/index.php/login">Login</a>
 </div>
 
 </body>
